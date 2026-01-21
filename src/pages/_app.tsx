19697030ba78c6feaa0f9e5174604b9790f0f6f1
Prateek Dwivedi <dprateek996@gmail.com>
2026-01-21 10:31:55 +0530

    adde tooltip on hover

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,12 +1,13 @@
 import "@/styles/globals.css";
 import type { AppProps } from "next/app";
 import { Analytics } from '@vercel/analytics/react';
+import { TooltipProvider } from "@/components/ui/tooltip";
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
-    <>
+    <TooltipProvider>
       <Component {...pageProps} />
       <Analytics />
-    </>
+    </TooltipProvider>
   );
 }
